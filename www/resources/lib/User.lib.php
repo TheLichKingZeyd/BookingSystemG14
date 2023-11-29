dfb538d54f50b6a2255cf0ed56e13af0ac3b2af6
ZumbaThirst <markus.fjelltun@hotmail.com>
2023-11-29 17:30:38 +0100

    Added password

--- www/resources/lib/User.lib.php
+++ www/resources/lib/User.lib.php
@@ -5,17 +5,19 @@ class User{
     public $userID;
     public $firstName;
     public $lastName;
+    public $password;
     public $isAssistant;
     public $email;
 
-    function __construct(int $userID, string $firstName, string $lastName, string $email, bool $isAssistant){
+    function __construct(int $userID, string $firstName, string $lastName, string $email, string $password, bool $isAssistant){
         $this->userID = $userID;
         $this->firstName = $firstName;
         $this->lastName = $lastName;
         $this->email = $email;
+        $this->password = $password;
         $this->isAssistant = $isAssistant;
     }
 
-};
+}
 
 ?>
