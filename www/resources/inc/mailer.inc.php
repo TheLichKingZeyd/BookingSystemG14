206eb1651b5e5e082bd113933900eda0fc3a8eb9
ZumbaThirst <markus.fjelltun@hotmail.com>
2023-12-05 16:19:25 +0100

    IT WORKS!!!!

--- www/resources/inc/mailer.inc.php
+++ www/resources/inc/mailer.inc.php
@@ -7,11 +7,6 @@ require __DIR__ . '/../lib/PHPMailer/src/Exception.php';
 require __DIR__ . '/../lib/PHPMailer/src/PHPmailer.php';
 require __DIR__ . '/../lib/PHPMailer/src/SMTP.php';
 
-include_once __DIR__ . '/../lib/booking.lib.php';
-//include_once __DIR__ . '/../lib/PHPMailer/src/Exception.php';
-//include_once __DIR__ . '/../lib/PHPMailer/src/PHPmailer.php';
-//include_once __DIR__ . '/../lib/PHPMailer/src/SMTP.php';
-
 
 // initialize PHPMailer and add variables that should be included for all mails
 function sendMails(array $mailInfo){
@@ -22,7 +17,7 @@ function sendMails(array $mailInfo){
     $mail->Port = 587;
     $mail->Host = "smtp.gmail.com";
     $mail->Username = "noreply.bsg14@gmail.com";
-    $mail->Password = "viioirzzdspumbob";
+    $mail->Password = "dvjkvwwjybujcnpc";
 
     // Send mails confirming a new booking
     if ($mailInfo['mailType'] == "Booking"){
@@ -41,13 +36,31 @@ function sendBookingConfirmationMails(PHPMailer $mail, array $mailInfo){
         $mail->Subject = "Booking confirmation";
 
         // construct email message for student about confirmed booking
-        $studMessage = "Hello " . $mailInfo['studName'] . ".<br><br>";
-        $studMessage .= "We have recieved your request for help from an assistant teacher. <br>br>";
-        $studMessage .= "You've chosen assistant teacher, " . $mailInfo['assistName'] . " , in " . $mailInfo['course'] . ". <br>";
+        $studMessage = "Hello " . $mailInfo['studName'] . ".\n\n";
+        $studMessage .= "We have recieved your request for help from an assistant teacher. \n";
+        $studMessage .= "You've chosen assistant teacher, " . $mailInfo['assistName'] . " , in " . $mailInfo['course'] . ". \n";
         $studMessage .= "The booking has been registered in the system.";
     
         // send email to student
         $mail->Body = $studMessage;
+        $mail->send();
+    } catch(Exception $exc){
+        echo "<script>window.location.href = 'booking.php'; alert('Mail not sent'" . $exc->getMessage() . ")</script>";
+    }
+    try {
+        // add variables to PHPMailer about the assistent
+        $mail->From = "noreply.bsg14@gmail.com";
+        $mail->FromName = "Noreply";
+        $mail->addAddress($mailInfo['assistEmail'], $mailInfo['assstName']);
+        $mail->Subject = "Booking confirmation";
+
+        // construct email message for assistant about new booking
+        $assistMessage = "Hello " . $mailInfo['assistName'] . ".\n\n";
+        $assistMessage .= "A student, " . $mailInfo['studName'] . " in " . $mailInfo['course'] . " has booked you for help.";
+        $assistMessage .= "You can find the booking in your calendar.";
+    
+        // send email to assistant
+        $mail->Body = $assistMessage;
         $mail->send();
     } catch(Exception $exc){
         echo "<script>window.location.href = 'booking.php'; alert('Mail not sent'" . $exc->getMessage() . ")</script>";
